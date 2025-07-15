63ced254f35f577eff287d8a22db31aad98b8df9
Addi3 <122154427+Addi3@users.noreply.github.com>
2025-07-15 01:37:07 +0100

    initial commit

---
+++ src/client/java/com/weaponized/WeaponizedClient.java
@@ -0,0 +1,10 @@
+package com.weaponized;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeaponizedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
