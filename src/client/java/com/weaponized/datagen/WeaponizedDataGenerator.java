18df70e1d97c059e68c826d41b9ea466dd7e0629
Loqor <isaiahcrace@gmail.com>
2025-07-16 16:50:25 -0500

    fix: library now works!!!!

--- src/client/java/com/weaponized/datagen/WeaponizedDataGenerator.java
+++ src/client/java/com/weaponized/datagen/WeaponizedDataGenerator.java
@@ -1,21 +1,27 @@
 package com.weaponized.datagen;
 
+import com.joyeuxlib.datagenproviders.JoyeuxLibEnglishLangProvider;
 import com.weaponized.datagen.providers.WeaponizedAchievementProvider;
-import com.weaponized.datagen.providers.WeaponizedEnglishLangProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
 
 public class WeaponizedDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-		pack.addProvider(WeaponizedEnglishLangProvider::new);
 		pack.addProvider(WeaponizedAchievementProvider::new);
+		pack.addProvider(this::englishTranslationsProvider);
 	}
 
-
-
-//	public void generateSoundData(FabricDataGenerator.Pack pack) {
-//		pack.addProvider((((output, registriesFuture) -> new (output))));
-//	}
+	public JoyeuxLibEnglishLangProvider englishTranslationsProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+		JoyeuxLibEnglishLangProvider joyENUSLang = new JoyeuxLibEnglishLangProvider(output, registriesFuture);
+		joyENUSLang.add("itemgroup.main","Weaponized");
+		joyENUSLang.add("item.weaponized.carrion_cleaver","Carrion Cleaver");
+		joyENUSLang.add("item.weaponized.carrion_cleaver.tooltip", "An old reliable cleaver, made of flesh and bones.");
+		return joyENUSLang;
+	}
 }
